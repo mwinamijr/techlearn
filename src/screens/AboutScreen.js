c84b4492e8910c85b5b733fdb2caefa91f393157
mwinamijr <mwinamijr@gmail.com>
2021-06-15 10:22:17 +0300

    Created about screen

---
+++ src/screens/AboutScreen.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Row, Col, Card } from 'react-bootstrap'
+
+
+function AboutScreen() {
+    return (
+        <div>
+          About
+        </div>
+    )
+}
+
+export default AboutScreen
