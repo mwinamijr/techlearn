733c182bd7bef34a64f6a0e8ec204390b14be376
mwinamijr <mwinamijr@gmail.com>
2022-08-06 16:41:30 +0300

    changed ReactDOM to use createRoot for react18

--- techlearn/frontend/src/App.js
+++ techlearn/frontend/src/App.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import { Container } from 'react-bootstrap'
 import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import Header from './components/Header'
@@ -12,22 +13,7 @@ import PrNotesList from './screens/notes/primary/PrNotesListScreen'
 
 function App() {
   return (
-    <Router>
-        <Header />
-      <main className="py-3">
-        <Routes>
-          <Route path='/' element={HomeScreen} exact />
-          <Route path='/login' element={LoginScreen} />
-          <Route path='/register' element={RegisterScreen} />
-          <Route path='/about' element={AboutScreen} />
-          <Route path='/notes/notes-list' element={NotesListScreen} />
-          <Route path='/notes/sec/notes-list' element={SecNotesList} />
-          <Route path='/notes/primary/notes-list' element={PrNotesList} />
-          
-        </Routes>
-      </main>
-      <Footer />
-    </Router>
+    <h1>React home</h1>
   );
 }
 
--- techlearn/frontend/src/index.js
+++ techlearn/frontend/src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 //import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(
-    <App />,
-  document.getElementById('app')
-);
+const container = document.getElementById('app');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<App />);
