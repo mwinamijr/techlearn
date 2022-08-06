8e7d477c478d0d56d18081c779e761e9064d41fc
mwinamijr <mwinamijr@gmail.com>
2022-08-06 16:44:00 +0300

    created users manager and models

--- techlearn/techlearn/urls.py
+++ techlearn/techlearn/urls.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 import debug_toolbar
 
@@ -7,3 +9,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('__debug__/', include(debug_toolbar.urls)),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- techlearn/users/admin.py
+++ techlearn/users/admin.py
@@ -2,18 +2,18 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
 from .forms import CustomUserCreationForm, CustomUserChangeForm
-from .models import CustomUser, Accountant, Teacher
+from .models import CustomUser
 
 
 class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = CustomUser
-    list_display = ('email', 'is_staff', 'is_active', 'is_accountant', 'is_teacher',)
-    list_filter = ('email', 'is_staff', 'is_active', 'is_accountant', 'is_teacher',)
+    list_display = ('email', 'is_staff', 'is_active', 'is_student', 'is_teacher',)
+    list_filter = ('email', 'is_staff', 'is_active', 'is_student', 'is_teacher',)
     fieldsets = (
         (None, {'fields': ('first_name', 'middle_name', 'last_name','email', 'password')}),
-        ('Permissions', {'fields': ('is_staff', 'is_active', 'is_accountant', 'is_teacher',)}),
+        ('Permissions', {'fields': ('is_staff', 'is_active', 'is_student', 'is_teacher',)}),
     )
     add_fieldsets = (
         (None, {
