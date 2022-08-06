91407a11ee1d45f6971775af85ffacec3dce8700
mwinamijr <mwinamijr@gmail.com>
2022-08-06 16:43:09 +0300

    fixed loading static files

--- techlearn/frontend/views.py
+++ techlearn/frontend/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 
-def index(request):
-    return render(request, 'frontend/index.html')
+def index(request, *args, **kwargs):
+    return render(request, 'index.html')
