7a1653afb0e5f5d91d0a673ee94d22cb5f4fe4fa
mwinamijr <mwinamijr@gmail.com>
2022-08-11 16:58:20 +0300

    changed to use redux instead of redux-toolkit

--- techlearn/frontend/src/store.js
+++ techlearn/frontend/src/store.js
@@ -1,14 +1,26 @@
-import { configureStore } from '@reduxjs/toolkit'
-import authReducer from './features/auth/authSlice'
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
+
+import {
+    userLoginReducer,
+} from './redux/reducers/userReducers'
+
+const reducer = combineReducers({
+    userLogin: userLoginReducer,
+})
 
 const userInfoFromStorage = localStorage.getItem('userInfo') ?
     JSON.parse(localStorage.getItem('userInfo')) : null
 
+const initialState = {
+    userLogin: { userInfo: userInfoFromStorage },
+}
 
-const store = configureStore({
-    reducer: {
-        auth: authReducer,
-    }
-})
+const middleware = [thunk]
+
+const store = createStore(reducer, initialState,
+    composeWithDevTools(applyMiddleware(...middleware),
+    ))
 
 export default store
