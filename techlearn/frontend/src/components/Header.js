ae5cb5fd4a0b6dd2a003ce32bef07e1d6df7dfbf
mwinamijr <mwinamijr@gmail.com>
2022-08-22 19:16:08 +0300

    added admin links to header

--- techlearn/frontend/src/components/Header.js
+++ techlearn/frontend/src/components/Header.js
@@ -2,63 +2,74 @@ import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Navbar, Nav, Container, Row, NavDropdown } from 'react-bootstrap'
 import { LinkContainer } from 'react-router-bootstrap'
-
+import SearchBox from './SearchBox'
 import { logout } from '../redux/actions/userActions'
 
 function Header() {
 
-  const dispatch = useDispatch()
+    const userLogin = useSelector(state => state.userLogin)
+    const { userInfo } = userLogin
 
-  const userLogin = useSelector(store => store.userLogin)
-  const { userInfo } = userLogin
-  
-  const logoutHandler = () => (
+    const dispatch = useDispatch()
+
+    const logoutHandler = () => {
       dispatch(logout())
-  )
-  return (
-    <header>
+    }
+
+    return (
+      <header>
         <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
           <Container>
-            <LinkContainer to='/'>
-              <Navbar.Brand>TechSchool</Navbar.Brand>
-            </LinkContainer>
+              <LinkContainer to='/'>
+                <Navbar.Brand>TechSchool</Navbar.Brand>
+              </LinkContainer>
 
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-                
-              <Nav className="mr-auto">
+              <Navbar.Toggle aria-controls="basic-navbar-nav" />
+              <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="ml-auto">
 
-                <LinkContainer to='/'>
-                    <Nav.Link >Home</Nav.Link>
-                </LinkContainer>
-                <LinkContainer to='/about'>
-                    <Nav.Link >About</Nav.Link>
-                </LinkContainer>
-                
-                <LinkContainer to='/contact-us'>
-                    <Nav.Link>Contact Us</Nav.Link>
-                </LinkContainer>
-                          
-              </Nav>
-              {userInfo
-              ? 
-                <Nav>
-                  <LinkContainer onClick={logoutHandler} className="float-right" to='/'>
-                    <Nav.Link ><i className=" float-right"></i>logout</Nav.Link>
+                  <LinkContainer to='/about'>
+                    <Nav.Link ><i className="fas fa-shopping-cart"></i>About</Nav.Link>
                   </LinkContainer>
-                </Nav>
-              :
-                <Nav>
-                  <LinkContainer className="float-right" to='/login'>
-                      <Nav.Link ><i className=" float-right"></i>Login</Nav.Link>
+                  <LinkContainer to='/contact-us'>
+                    <Nav.Link ><i className="fas fa-shopping-cart"></i>Contact Us</Nav.Link>
                   </LinkContainer>
+
+                  {userInfo ? (
+                    <NavDropdown title={userInfo.name} id='username'>
+                      <LinkContainer to='/profile'>
+                        <NavDropdown.Item>Profile</NavDropdown.Item>
+                      </LinkContainer>
+
+                      <NavDropdown.Item onClick={logoutHandler}>Logout</NavDropdown.Item>
+
+                    </NavDropdown>
+                  ) : (
+                    <LinkContainer to='/login'>
+                      <Nav.Link><i className="fas fa-user"></i>Login</Nav.Link>
+                    </LinkContainer>
+                  )}
+
+
+                  {userInfo && userInfo.isAdmin && (
+                    <NavDropdown title='Admin' id='adminmenu'>
+                        <LinkContainer to='/admin/userlist'>
+                          <NavDropdown.Item>Users</NavDropdown.Item>
+                        </LinkContainer>
+
+                        <LinkContainer to='/admin/productlist'>
+                          <NavDropdown.Item>Products</NavDropdown.Item>
+                        </LinkContainer>
+
+                    </NavDropdown>
+                  )}
+
                 </Nav>
-              }
-            </Navbar.Collapse>
+              </Navbar.Collapse>
           </Container>
         </Navbar>
-    </header>
-  )
+      </header>
+    )
 }
 
 export default Header
